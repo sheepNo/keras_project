a803e3b855f4cc30ffa575d06934c3c3ab8f639a
Etienne Bontemps <etienne.bontemps@ensimag.fr>
2018-04-02 17:28:34 +0200

    loadmodel.py ud

--- loadmodel.py
+++ loadmodel.py
@@ -3,14 +3,13 @@
 Our implementation
 """
 
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, load_model
 from keras.layers import Input, Dense, Dropout, Activation, Conv2D, Flatten, MaxPooling2D, AveragePooling2D, GaussianDropout
 from keras.optimizers import *
 from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.normalization import BatchNormalization
-from keras.models import load_model
 
 import numpy as np
 
@@ -25,15 +24,10 @@ datagen = ImageDataGenerator(
             zoom_range=0.1,
             shear_range=0.1)
 
-# cheating
-# if K.image_data_format() == 'channels_first':
 data = data.reshape(data.shape[0], img_rows, img_cols, 1)
 test = test.reshape(test.shape[0], img_rows, img_cols, 1)
 input_shape = (1, img_rows, img_cols)
-# else:
-    # x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    # x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    # input_shape = (img_rows, img_cols, 1)
+
 
 labels, testlabels = to_categorical(labels, 10), to_categorical(testlabels, 10)
 
@@ -60,15 +54,12 @@ model.add(Flatten())
 #####################################$
 model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', # mean_squared_error
+model.compile(loss='categorical_crossentropy',
             optimizer='adagrad',
             metrics=['accuracy'])
 
-#
-# model.fit_generator(datagen.flow(data, labels, batch_size=200), epochs=16, validation_data=(test, testlabels))
 
-
-model.load_weights('2h995.h5')  # creates a HDF5 file 'my_model.h5'
+model.load_weights('2h995.h5')
 
 
 score = model.evaluate(test, testlabels, verbose=1)
